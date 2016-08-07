c118769cdc8795803204e406b13bb0ee92a173cf
liuhongliang <1029765111@qq.com>
2016-08-07 19:36:04 +0800

    添加单元测试

---
+++ src/Jeuci.SalesSystem.EntityFramework/Migrations/SeedData/InitialDbBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework.DynamicFilters;
+using Jeuci.SalesSystem.EntityFramework;
+
+namespace Jeuci.SalesSystem.Migrations.SeedData
+{
+    public class InitialDbBuilder
+    {
+        private readonly SalesSystemDbContext _context;
+
+        public InitialDbBuilder(SalesSystemDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create()
+        {
+            _context.DisableAllFilters();
+
+            new DefaultEditionCreator(_context).Create();
+            //new DefaultLanguagesCreator(_context).Create();
+            //new DefaultTenantRoleAndUserCreator(_context).Create();
+            //new DefaultSettingsCreator(_context).Create();
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/Jeuci.SalesSystem.EntityFramework/Migrations/SeedData/TestDataCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jeuci.SalesSystem.EntityFramework;
+
+namespace Jeuci.SalesSystem.Migrations.SeedData
+{
+    public class TestDataCreator
+    {
+        private SalesSystemDbContext _context;
+
+        public TestDataCreator(SalesSystemDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create()
+        {
+
+        }
+    }
+}
